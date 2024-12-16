7ba544db12e3c73ecd93ef92ec459409bf47302e
Wayne <smasedon107@gmail.com>
2024-12-16 22:17:02 +0800

    start script

--- config/db.js
+++ config/db.js
@@ -1,10 +1,18 @@
-// config/db.js
 const mongoose = require("mongoose");
 
-const connectDB = async (uri) => {
+const connectDB = async () => {
   try {
+    const uri =
+      process.env.NODE_ENV === "production"
+        ? process.env.PRODUCTION_MONGODB_URI
+        : process.env.LOCAL_MONGODB_URI;
+
     await mongoose.connect(uri);
-    console.log("MongoDB 連接成功");
+    console.log(
+      `MongoDB: ${
+        process.env.NODE_ENV === "production" ? "Atlas" : "Local"
+      } 資料庫連接成功`
+    );
   } catch (err) {
     console.error("MongoDB 連接失敗:", err.message);
     process.exit(1);
